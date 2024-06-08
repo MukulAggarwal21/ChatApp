08cbfcc49445c8af050fcf7c57a16e25bc73f6fa
MUKUL <mukul.2226csit1115@kiet.edu>
2024-06-08 23:18:32 +0530

    chats upgrade

--- index.js
+++ index.js
@@ -7,22 +7,100 @@ const { Server } = require("socket.io");
 
 const app = express()
 const server = http.createServer(app);
-const io = new Server(server);
+const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
+const { availableParallelism } = require('node:os');
+const cluster = require('node:cluster');
+const { createAdapter, setupPrimary } = require('@socket.io/cluster-adapter');
 
 
-//Socket io 
-//server side 
-io.on('connection', (socket) => {
-    socket.on('message', (message) => {
-        io.emit("message", message) //if message is come from server then gave it to everybody 
+
+if (cluster.isPrimary) {
+  const numCPUs = availableParallelism();
+  console.log(numCPUs);
+  // create one worker per available core
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork({
+      PORT: 3000 + i
     });
+  }
+  // set up the adapter on the primary thread
+  return setupPrimary();
+}
 
-});
+async function main() {
+  // open the database file
+  const db = await open({
+    filename: 'chat.db',
+    driver: sqlite3.Database
+  });
 
-app.use(express.static(path.resolve('./public')));
-app.get("/", (req, res) => {
+  // create our 'messages' table (you can ignore the 'client_offset' column for now)
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_offset TEXT UNIQUE,
+        content TEXT
+    );
+  `);
+
+  const io = new Server(server, {
+    connectionStateRecovery: {},
+    // set up the adapter on each worker thread
+    adapter: createAdapter()
+  });
+
+  //Socket io 
+  //server side 
+  io.on('connection', async (socket) => {
+    socket.on('message', async (message, clientOffset, callback) => {
+      let result;
+      try {
+        result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?, ?)', message, clientOffset);
+      } catch (e) {
+        if (e.errno === 19 /* SQLITE_CONSTRAINT */) {
+          // the message was already inserted, so we notify the client
+          if (typeof callback === 'function') {
+            callback();
+          }
+        } else {
+          // nothing to do, just let the client retry
+        }
+        return;
+      }
+      io.emit('message', message, result.lastID);
+      // acknowledge the event
+      if (typeof callback === 'function') {
+        callback();
+      }
+    });
+    if (!socket.recovered) {
+      // if the connection state recovery was not successful
+      try {
+        await db.each('SELECT id, content FROM messages WHERE id > ?',
+          [socket.handshake.auth.serverOffset || 0],
+          (_err, row) => {
+            socket.emit('message', row.content, row.id);
+          }
+        )
+      } catch (e) {
+        // something went wrong
+      }
+    }
+
+
+  });
+
+  app.use(express.static(path.resolve('./public')));
+  app.get("/", (req, res) => {
     return res.sendFile("/public/index.html")
-})
+  })
 
-server.listen(9000, () => console.log(`server started at PORT : 9000`))
+  const port = process.env.PORT;
 
+  server.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`);
+  });
+
+}
+main();
